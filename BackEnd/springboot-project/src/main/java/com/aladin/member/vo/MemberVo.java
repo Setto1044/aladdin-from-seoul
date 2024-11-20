8f672e942b6547491061e932c77b9a5f39def61d
배석진 <baesj1044@gmail.com>
2024-11-20 17:50:40 +0900

    💩매물 게시판 패키지 구조화
    Former-commit-id: 99c0c3a0fcff04fb43174530de31275af49b8a63

--- BackEnd/springboot-project/src/main/java/com/aladin/member/vo/MemberVo.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/member/vo/MemberVo.java
@@ -11,17 +11,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class MemberVo {
-	private Long id;					// DB 기본키
-	private String username;			// 아이디
-	private String password;			// 비밀번호
-	private String name;				// 실명
-	private String nickname;			// 별명
-	private String email;				// 이메일
-	private String profileImagePath;	// 프로필 사진 URL
-	private MemberGrade grade;			// 등급(NORMAL, BUSINESS, ADMIN)
-	private MemberStatus status;		// 상태(ACTIVE, SUSPENDED, DELETED)
-	private LocalDateTime createdAt;	// 회원 가입일
-	private LocalDateTime lastjoinedAt;	// 마지막 로그인
-	private LocalDateTime deletedAt;	// 회원 삭제일
-	private String bio;					// 소개글
+	private Long id; // DB 기본키
+	private String username; // 아이디
+	private String password; // 비밀번호
+	private String name; // 실명
+	private String nickname; // 별명
+	private String email; // 이메일
+	private String profileImagePath; // 프로필 사진 URL
+	private MemberGrade grade; // 등급(NORMAL, BUSINESS, ADMIN)
+	private MemberStatus status; // 상태(ACTIVE, SUSPENDED, DELETED)
+	private LocalDateTime createdAt; // 회원 가입일
+	private LocalDateTime lastjoinedAt; // 마지막 로그인
+	private LocalDateTime deletedAt; // 회원 삭제일
+	private String bio; // 소개글
 }
