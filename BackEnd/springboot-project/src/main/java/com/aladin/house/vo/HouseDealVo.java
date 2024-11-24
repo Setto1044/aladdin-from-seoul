7af71fb11e579f86b023ea456fd81f71f9c5c6fc
배석진 <bsj1044@naver.com>
2024-11-24 12:25:39 +0900

    🐛 아파트 거래 통계 쿼리 stat 테이블 참조하도록 수정
    Former-commit-id: 214c239ff927ccea01fe1c171b4b8c93fa8eed6d

--- BackEnd/springboot-project/src/main/java/com/aladin/house/vo/HouseDealVo.java
+++ BackEnd/springboot-project/src/main/java/com/aladin/house/vo/HouseDealVo.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class HouseDealVo {
-	private Long no;              // 거래 고유 번호
-	private String aptSeq;        // 아파트 시퀀스
-	private String aptDong;       // 아파트 동 이름
-	private String floor;         // 층수
-	private Integer dealYear;     // 거래 연도
-	private Integer dealMonth;    // 거래 월
-	private Integer dealDay;      // 거래 일
-	private Double excluUseAr;    // 전용 면적
-	private String dealAmount;    // 거래 금액
+	private Long no; // 거래 고유 번호
+	private String aptSeq; // 아파트 시퀀스
+	private String aptDong; // 아파트 동 이름
+	private String floor; // 층수
+	private Integer dealYear; // 거래 연도
+	private Integer dealMonth; // 거래 월
+	private Integer dealDay; // 거래 일
+	private Double excluUseAr; // 전용 면적
+	private String dealAmount; // 거래 금액
+	private Integer views; // 조회수
 }
